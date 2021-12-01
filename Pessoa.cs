d0c36aae7bfe38756972702496a2eb7ae5889372
Pamera-png <pameranatali@gmail.com>
2021-12-01 09:13:49 -0300

    commit orientação a objeto

---
+++ Pessoa.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace ProgramacaoOrientadaObjeto.Cadastros{
+
+ public class Pessoa{
+
+     //Essas são as propriedades. Os nomes de propriedades podem ser qualquer nome
+public int Codigo { get; set; }
+public String? Nome { get; set; }
+public String? Sobrenome { get; set; }
+public DateTime DataNascimento { get; set; }
+
+//Esse é o método construtor. Método construtor tem o mesmo nome da classe
+public Pessoa(){}
+//essa é a sobrecarga do método construtor. Quando usamos o método com mesmo nome e parâmetros diferentes
+public Pessoa(String Nome, String Sobrenome, DateTime dataNascimento){
+
+//this acessa os membros de dentro da própria classe
+this.Nome = Nome;
+this.Sobrenome = Sobrenome;
+this.DataNascimento = DataNascimento;
+}
+ }   
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+//se a outra classe tiver o name space diferente use a linha a baixo para referenciar
+using ProgramacaoOrientadaObjeto.Cadastros;
+
+namespace ProgramacaoOrientadaObjeto{
+    public class Program    {
+
+        public static void Main(string[] args)        {
+            //para criar um objeto digite o nome da classe que você quer referenciar, o nome do objeto, sinal de = e a palavra new();
+Pessoa objetoPessoa = new();
+objetoPessoa.Nome = "João";
+objetoPessoa.Sobrenome = "Silva";
+Console.WriteLine($"o nome da pessoa é {objetoPessoa.Nome} {objetoPessoa.Sobrenome}");
+
+//passando parâmetro da sobrecarga do construtor
+Pessoa novoObjetoPessoa = new("Maria", "Silva", DateTime.Now);
+Console.WriteLine($"o nome da pessoa do novo objeto é {novoObjetoPessoa.Nome} {novoObjetoPessoa.Sobrenome}");
+}
+}
+}
